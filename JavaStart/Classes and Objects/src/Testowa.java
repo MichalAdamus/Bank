cf62c7023551a0caded0dd32b8fcf46ff7a608b1
Michał Adamus <mb.adamus@gmail.com>
2022-05-26 12:14:24 +0200

    Dodanie pliku testowego z Hello world i liczbą 3

---
+++ JavaStart/Classes and Objects/src/Testowa.java
@@ -0,0 +1,9 @@
+public class Testowa {
+    public static void main(String[] args) {
+        int DowolnaLiczba = 3;
+        String Nazwa = "Hello world";
+
+        System.out.println(Nazwa);
+        System.out.println(DowolnaLiczba);
+    }
+}
